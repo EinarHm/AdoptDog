2f8b47c8884fae175d94d9969720b51450e25ac0
EinarHm <eihoma94@gmail.com>
2021-02-24 16:00:52 -0600

    Adding dog list

--- screens/Home.js
+++ screens/Home.js
@@ -1,18 +1,60 @@
-import { Text, View, StyleSheet } from "react-native";
-import * as React from "react";
+import { Text, View, StyleSheet, Image, ActivityIndicator } from "react-native";
+import React, { useState, useEffect } from "react";
 
 function HomeScreen() {
-  return (
+  const [data, setData] = useState({ img: [], isLoading: true });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let response = await fetch("https://dog.ceo/api/breeds/image/random/8");
+      let responseJson = await response.json();
+      setData({ img: responseJson.message, isLoading: false });
+    };
+    fetchData();
+  }, []);
+
+  return data.isLoading === true ? (
+    <View style={{ flex: 1, padding: 20 }}>
+      <ActivityIndicator />
+    </View>
+  ) : (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
+      <View style={styles.gridContainer}>
+        {data.img.map((image) => (
+          <View style={styles.item}>
+            <Image
+              style={styles.image}
+              source={{
+                uri: image,
+              }}
+            />
+            <Text>Guardar</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    margin: 8,
+  },
+  gridContainer: {
     flex: 1,
-    paddingTop: Platform.OS === "android" ? 35 : 0,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "flex-start", // if you want to fill rows left to right},
+  },
+  item: {
+    width: "50%",
+    height: 150,
+
+    alignItems: "center",
+  },
+  image: {
+    width: "100%",
+    height: 120,
   },
 });
 
