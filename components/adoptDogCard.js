8be617ff018c3672d15e0e0db8136f087f7941b0
EinarHm <eihoma94@gmail.com>
2021-02-24 16:50:33 -0600

    Refactoring Home into adoptDogCard.js

---
+++ components/adoptDogCard.js
@@ -0,0 +1,51 @@
+import {
+  TouchableOpacity,
+  Text,
+  View,
+  Image,
+  Alert,
+  StyleSheet,
+} from "react-native";
+import React from "react";
+function adoptDogCard(props) {
+  return (
+    <View style={styles.item}>
+      <Image
+        style={styles.image}
+        source={{
+          uri: props.image,
+        }}
+      />
+      <TouchableOpacity
+        style={styles.adoptButton}
+        onPress={() => Alert.alert("Thank you for adopting this dog")}
+      >
+        <Text style={styles.adoptButtonText}>ADOPT</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  item: {
+    width: "50%",
+    height: 180,
+    alignItems: "center",
+    marginBottom: 5,
+  },
+  image: {
+    width: "100%",
+    height: 150,
+  },
+  adoptButton: {
+    width: "50%",
+    marginTop: 4,
+    borderRadius: 30,
+    backgroundColor: "#f35764",
+    alignItems: "center",
+  },
+  adoptButtonText: {
+    color: "#F9F9F9",
+  },
+});
+export default adoptDogCard;
--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,7 @@
-import {
-  TouchableOpacity,
-  Text,
-  View,
-  StyleSheet,
-  Image,
-  ActivityIndicator,
-  Alert,
-  ScrollView,
-} from "react-native";
+import { View, StyleSheet, ActivityIndicator, ScrollView } from "react-native";
 import React, { useState, useEffect } from "react";
 
+import AdoptDogCard from "../components/adoptDogCard";
 function HomeScreen() {
   const [data, setData] = useState({ img: [], isLoading: true });
 
@@ -31,20 +23,7 @@ function HomeScreen() {
       <View style={styles.container}>
         <View style={styles.gridContainer}>
           {data.img.map((image) => (
-            <View style={styles.item}>
-              <Image
-                style={styles.image}
-                source={{
-                  uri: image,
-                }}
-              />
-              <TouchableOpacity
-                style={styles.adoptButton}
-                onPress={() => Alert.alert("Thank you for adopting this dog")}
-              >
-                <Text style={styles.adoptButtonText}>ADOPT</Text>
-              </TouchableOpacity>
-            </View>
+            <AdoptDogCard image={image} />
           ))}
         </View>
       </View>
@@ -60,27 +39,7 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "row",
     flexWrap: "wrap",
-    alignItems: "flex-start", // if you want to fill rows left to right},
-  },
-  item: {
-    width: "50%",
-    height: 180,
-    alignItems: "center",
-    marginBottom: 5,
-  },
-  image: {
-    width: "100%",
-    height: 150,
-  },
-  adoptButton: {
-    width: "50%",
-    marginTop: 4,
-    borderRadius: 30,
-    backgroundColor: "#f35764",
-    alignItems: "center",
-  },
-  adoptButtonText: {
-    color: "#F9F9F9",
+    alignItems: "flex-start",
   },
 });
 
